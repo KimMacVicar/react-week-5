48bfed0237527e266c57c4e2ec540a0fdb02f8e6
KimMacVicar <92276525+KimMacVicar@users.noreply.github.com>
2022-05-08 21:15:20 -0600

    re-added bootstrap

---
+++ src/WeatherForecast.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "./WeatherForecast.css";
+
+export default function WeatherForecast() {
+  return (
+    <div className="WeatherForecastApp">
+      <div className="row">
+        <div className="col-2 forecastDays">WeatherForecast</div>
+        <div className="col-2 forecastDays">WeatherForecast</div>
+        <div className="col-2 forecastDays">WeatherForecast</div>
+        <div className="col-2 forecastDays">WeatherForecast</div>
+        <div className="col-2 forecastDays">WeatherForecast</div>
+        <div className="col-2 forecastDays">WeatherForecast</div>
+      </div>
+    </div>
+  );
+}
